2a43532fd685ddca752eec7f0f624c927f38f35f
zxchengb <zxchengb@163.com>
2020-08-29 09:12:27 +0800

    Update web content config.

--- web/public/config.js
+++ web/public/config.js
@@ -3,11 +3,11 @@
 if (window.ZEle === undefined) {
   window.ZEle = {};
 }
-//window.ZEle.endpoint = "http://192.168.0.1:8080";
+//window.ZEle.endpoint = "http://dev.cloud.smallsaas.cn:8787";
 window.ZEle.endpoint = "http://localhost:8080";
-window.ZEle.nav = "top";
+window.ZEle.nav = "left";
 window.ZEle.theme = "#1890ff";
-window.ZEle.indexPage = "/test";
+window.ZEle.indexPage = "/login";
 window.ZEle.breadcrumb = false;
 window.ZEle.remoteConfig = {};
 
--- web/src/config/router.config.js
+++ web/src/config/router.config.js
@@ -1,32 +1,3 @@
 module.exports = [
- /* {
-    name: '店铺管理',
-    path: '/studio',
-    items: [
-      {
-        path: '/studio/studios',
-        name: '店铺列表',
-      },
-    ],
-  },
-  {
-    name: '用户管理',
-    path: '/customers',
-    items: [
-      {
-        path: '/customers/customer',
-        name: '用户列表',
-      },
-    ]
-  }
-  {
-    name: '一级菜单',
-    path: '/b',
-    items: [
-      {
-        path: '/b/c',
-        name: '二级菜单',
-      },
-    ],
-  },*/
+
 ]
