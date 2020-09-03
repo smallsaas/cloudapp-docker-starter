25cbf3b9e6e1deae4f131dca033634ce1d94e090
zxchengb <zxchengb@163.com>
2020-09-03 18:37:00 +0800

    Optimize script file.

--- script/docker-compose-install.py
+++ script/docker-compose-install.py
@@ -2,4 +2,4 @@
 import os
 os.system('curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -')
 os.system('curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose')
-os.system('chmod +x /usr/local/bin/docker-compose')
+os.system('sudo chmod +x /usr/local/bin/docker-compose')
