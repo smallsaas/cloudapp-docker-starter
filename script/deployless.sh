11d4dbfa1d4dc9a175ab8220fc3bb8e33db5ead8
zxchengb <zxchengb@163.com>
2020-09-05 13:32:03 +0800

    Increase '-s --ssh' option to realize secret-free verification and update the '资源管理.md' at the same time.

--- script/deployless.sh
+++ script/deployless.sh
@@ -6,31 +6,58 @@ target='root@server_ip:/root/dev/api/'
 force='^-f$'
 delete='^-d$'
 list='^-l$'
+ssh='^-s$'
 #### split from target  below ###
 app_path=${target##*:}
 ssh_host=${target%%:*}
 ####
 
+## deploy lib
+deploy_lib() {
+   echo scp $(readlink -f $jar) ${target}/lib
+   scp $(readlink -f $jar) ${target}/lib
+   echo ssh $ssh_host \"cd $app_path exec sh docker-deploy-lib.sh $option\"
+   ssh $ssh_host "cd $app_path && sh docker-deploy-lib.sh $option"
+}
+
+ssh_copy_id() {
+   rsa_path='.ssh\\id_rsa.pub'
+   cd ~
+   which "ssh-copy-id -h" >/dev/null 2>&1
+   if [ $? -eq 1 ]; then
+      if [ ! -f $rsa_path ]; then
+         ssh-keygen -t rsa
+      fi
+      echo ssh-copy-id -i ~/.ssh/id_rsa.pub $ssh_host
+      ssh-copy-id -i ~/.ssh/id_rsa.pub $ssh_host
+   else
+      echo 'Please install Git tool.'
+   fi
+   exit
+}
+
 if [ ! $jar ]; then
    echo 'Usage: deployless <jarFile>'
    echo '  e.g. deployless test.jar'
-   echo '  -d  --delete 删除资源包'
-   echo '  -f  --force  强制装配资源包'
+   echo '  -d  --delete <jarName> 删除资源包'
+   echo '  -f  --force  <jarFilePath> 强制装配资源包'
    echo '  -l  --list   显示云端已装配资源包列表'
+   echo '  -s  --ssh    保存本地ssh的公共密钥至云端'
    exit
 fi
 
-if [[ "$option" =~ $force ]] || [[ "$option" =~ $delete ]] || [[ "$option" =~ $list ]]; then
+if [[ "$option" =~ $force || "$option" =~ $delete || "$option" =~ $list || "$option" =~ $ssh ]]; then
    jar=$2
    if [[ "$option" =~ $delete ]]; then
       echo ssh $ssh_host \"cd $app_path exec sh docker-deploy-lib.sh $option $jar\"
       ssh $ssh_host "cd $app_path && sh docker-deploy-lib.sh $option $jar"
       exit
-   fi
-   if [[ "$option" =~ $list ]]; then
+   elif [[ "$option" =~ $list ]]; then
       echo ssh $ssh_host \"cd $app_path exec sh docker-deploy-lib.sh $option\"
       ssh $ssh_host "cd $app_path && sh docker-deploy-lib.sh $option"
       exit
+   elif [[ "$option" =~ $ssh ]]; then
+      ssh_copy_id
    fi
 fi
 
@@ -43,14 +70,6 @@ if [[ ! $jar =~ .jar$ ]]; then
    echo $jar isn\'t a jar file.
    exit
 fi
-
-## deploy lib
-deploy_lib() {
-   echo scp $(readlink -f $jar) ${target}/lib
-   scp $(readlink -f $jar) ${target}/lib
-   echo ssh $ssh_host \"cd $app_path exec sh docker-deploy-lib.sh $option\"
-   ssh $ssh_host "cd $app_path && sh docker-deploy-lib.sh $option"
-}
 
 deploy_lib
 # done
--- script/deployless_pages.sh
+++ script/deployless_pages.sh
@@ -1,36 +1,16 @@
 #!/usr/bin/bash
-
+## host ##
+target='root@server_ip:/root/dev/web/'
 mod=$1
 routerName=$2
 routerPath=$3
 delete='^-d$'
 list='^-l$'
-
-## host ##
-target='root@server_ip:/root/dev/web/'
+ssh='^-s$'
 #### split from target  below ###
 app_path=${target##*:}
 ssh_host=${target%%:*}
 ####
-
-if [[ "${mod}" =~ $delete ]] || [[ "${mod}" =~ $list ]]; then
-   echo ssh $ssh_host \"cd $app_path and exec docker-deploy-page.sh $*\"
-   ssh $ssh_host "cd $app_path && sh docker-deploy-page.sh $*"
-   exit
-fi
-
-if [ ! $mod ] || [ ! $routerName ] || [ ! $routerPath ]; then
-   echo 'Usage: bash deployless_pages.sh <page_path> <route_name> <router_path>'
-   echo '  e.g. bash deployless_pages.sh web/src/pages/page_test 菜单名称 page_test'
-   echo '  -d  --delete 删除页面'
-   echo '  -l   --list 页面列表'
-   exit
-fi
-
-if [ ! -d $(readlink -f $mod) ]; then
-   echo page $mod not exists
-   exit
-fi
 
 ## deploy page
 deploy_page() {
@@ -39,7 +19,6 @@ deploy_page() {
       echo you try to deply page, but config not exists
       exit
    fi
-
    ## package page with tar
    echo tar -cvf ${mod}.tar ${mod}
    tar -cvf $(readlink -f $mod).tar ${mod}
@@ -52,6 +31,46 @@ deploy_page() {
    echo ssh $ssh_host \"cd $app_path and exec docker-deploy-page.sh\"
    ssh $ssh_host "cd $app_path && sh docker-deploy-page.sh $mod $routerName $routerPath"
 }
+
+ssh_copy_id() {
+   rsa_path='.ssh\\id_rsa.pub'
+   cd ~
+   which "ssh-copy-id -h" >/dev/null 2>&1
+   if [ $? -eq 1 ]; then
+      if [ ! -f $rsa_path ]; then
+         ssh-keygen -t rsa
+      fi
+      echo ssh-copy-id -i ~/.ssh/id_rsa.pub $ssh_host
+      ssh-copy-id -i ~/.ssh/id_rsa.pub $ssh_host
+   else
+      echo 'Please install Git tool.'
+   fi
+   exit
+}
+
+if [[ "${mod}" =~ $delete || "${mod}" =~ $list ]]; then
+   echo ssh $ssh_host \"cd $app_path and exec docker-deploy-page.sh $*\"
+   ssh $ssh_host "cd $app_path && sh docker-deploy-page.sh $*"
+   exit
+fi
+
+if [[ "${mod}" =~ $ssh ]]; then
+   ssh_copy_id
+fi
+
+if [ ! $mod || ! $routerName || ! $routerPath ]; then
+   echo 'Usage: bash deployless_pages.sh <page_path> <route_name> <router_path>'
+   echo '  e.g. bash deployless_pages.sh web/src/pages/page_test 菜单名称 page_test'
+   echo '  -d  --delete <pageName> 删除页面'
+   echo '  -l  --list   页面列表'
+   echo '  -s  --ssh    保存本地ssh的公共密钥至云端'
+   exit
+fi
+
+if [ ! -d $(readlink -f $mod) ]; then
+   echo page $mod not exists
+   exit
+fi
 
 ## main  ##
 if [ -f $(readlink -f $mod)/index.js ]; then
